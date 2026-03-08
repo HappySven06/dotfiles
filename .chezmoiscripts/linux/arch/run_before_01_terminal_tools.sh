416af0fe067ff00bbd3e8f2a34f978935391e055
Sven Laht <sven.laht@gmail.com>
2026-03-08 21:38:22 +0200

    Update .chezmoiscripts/linux/arch/01_terminal_tools.sh Update .bashrc

--- .chezmoiscripts/linux/arch/run_before_01_terminal_tools.sh
+++ .chezmoiscripts/linux/arch/run_before_01_terminal_tools.sh
@@ -1,3 +1,40 @@
 #!/bin/bash
 
-echo "Hello"
+# Format: ["command_to_check"]="package_name"
+declare -A install_packages=(
+    [micro]="micro"
+    [ttf-jetbrains-mono-nerd]="ttf-jetbrains-mono-nerd"
+    [starship]="starship"
+    [eza]="eza"
+    [bat]="bat"
+    [zoxide]="zoxide"
+    [fzf]="fzf"
+)
+
+# Packages to remove (just package names)
+remove_packages=()
+
+echo "[INSTALL] Installing terminal tools..."
+
+# Install packages
+for cmd in "${!install_packages[@]}"; do
+    pkg="${install_packages[$cmd]}"
+    if ! command -v "$cmd" &>/dev/null; then
+        echo "[INSTALL] Installing $pkg (provides $cmd)..."
+        sudo pacman -S --noconfirm "$pkg"
+    else
+        echo "[INFO] $cmd already available"
+    fi
+done
+
+# Remove unwanted packages
+for pkg in "${remove_packages[@]}"; do
+    if pacman -Q "$pkg" &>/dev/null; then
+        echo "[REMOVE] Removing $pkg..."
+        sudo pacman -R --noconfirm "$pkg"
+    else
+        echo "[INFO] $pkg is not installed"
+    fi
+done
+
+echo "[SUCCESS] Terminal tools installed successfully!"
